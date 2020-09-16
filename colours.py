5fed92f5b4b0ba9f9a92b3a3070cc156f4e49355
Peter McCauley <pmccauley@tradevela.com>
2020-09-16 20:26:16 +0100

    Simplified line checks, added colours file

---
+++ colours.py
@@ -0,0 +1,4 @@
+default = ["red", "white", "blue", "green", "yellow", "pink"]
+pastel = ["#FEBCC8", "#FFFFD8", "#EAEBFF", "#E0FEFE", "#D3EEFF"]
+pastel_dark = ["#C23B23", "#F39A27", "#EADA52", "#03C03C", "#579ABE", "#976ED7"]
+teal = ["#005660", "#007475", "#008081", "#229389", "#34A798", "#57C3AD"]
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import colours
+
 import random
 from PIL import Image, ImageDraw
 
@@ -21,94 +23,78 @@ def ring(draw, x, y, size, thickness=2, outline="black", fill="white"):
     draw.ellipse([x1, y1, x2, y2], fill=outline)
 
     # inner circle
-    x1 = x + thickness/2
-    x2 = x + size - thickness/2
-    y1 = y + thickness/2
-    y2 = y + size - thickness/2
-    draw.ellipse([x1, y1, x2, y2], fill=fill)
+    # only draw if we need to
+    if fill is not None:
+        x1 = x + thickness/2
+        x2 = x + size - thickness/2
+        y1 = y + thickness/2
+        y2 = y + size - thickness/2
+        draw.ellipse([x1, y1, x2, y2], fill=fill)
 
 
-colours = ["red", "white", "blue", "green", "yellow", "pink"]
+background = "#222222"
+fill = "white"
+
+
+# TODO - move this into a module
+# TODO - allow passing in a screen size
+# TODO - allow passing in colours
+# TODO - allow passing in colour groups i.e. pastel
+# TODO - allow passing in numbers (plus different bases???)
+# TODO - allow disabling of diagonals or only diagonals
 
 
 def main():
-    diameter = 20
-    radius = 10
-    spacing = 15
-    thickness = 10
+    diameter = 30
+    radius = diameter / 2
+    spacing = 20
+    thickness = 16  # ignored if fill is none
+    line_thickness = 5
     x_number = 50
-    y_number = 25
+    y_number = 20
 
     x_size = x_number * (diameter + spacing) + spacing
     y_size = y_number * (diameter + spacing) + spacing
 
-    img = Image.new('RGB', (x_size, y_size), color='black')
-
+    img = Image.new('RGB', (x_size, y_size), color=background)
     d = ImageDraw.Draw(img)
 
     points = [
         [
             {
-                "colour": random.choice(colours),
+                "colour": random.choice(colours.teal),
                 "x": (x * (diameter + spacing) + spacing),
                 "y": (y * (diameter + spacing)) + spacing
             } for y in range(y_number)
         ] for x in range(x_number)
     ]
 
-    # get out lines
+    # get our lines to be drawn
     lines = []
 
     # do this by indexing, easier than iterating over the list
-    # need to only check down, right and the bottom diagonal
+    # need to only check down, right, RD diagonal and RU diagonal
     for r in range(len(points)):
         for c in range(len(points[0])):  # just check the len of the first row, all the same diameter
-            # checking right
-            try:
-                if points[r][c]["colour"] == points[r][c+1]["colour"]:
-                    lines.append((
-                        (points[r][c]["x"] + radius, points[r][c]["y"] + radius),
-                        (points[r][c+1]["x"] + radius, points[r][c+1]["y"] + radius),
-                        points[r][c]["colour"]
-                    ))
-            except IndexError:
-                pass
+            # R, D, RD, RU
+            for r_, c_ in [(r, c+1), (r+1, c), (r+1, c+1), (r-1, c+1)]:
+                try:
+                    # guard around negative indexing
+                    if r_ < 0:
+                        continue
 
-            # checking down
-            try:
-                if points[r][c]["colour"] == points[r+1][c]["colour"]:
-                    lines.append((
-                        (points[r][c]["x"] + radius, points[r][c]["y"] + radius),
-                        (points[r+1][c]["x"] + radius, points[r+1][c]["y"] + radius),
-                        points[r][c]["colour"]
-                    ))
-            except IndexError:
-                pass
-
-            # rd diagonal
-            try:
-                if points[r][c]["colour"] == points[r+1][c+1]["colour"]:
-                    lines.append((
-                        (points[r][c]["x"] + radius, points[r][c]["y"] + radius),
-                        (points[r+1][c+1]["x"] + radius, points[r+1][c+1]["y"] + radius),
-                        points[r][c]["colour"]
-                    ))
-            except IndexError:
-                pass
-
-            # ru diagonal
-            try:
-                # for the ru diagonal we need an extra guard for negative indexes
-                if points[r][c]["colour"] == points[r-1][c+1]["colour"] and r - 1 > 0:
-                    lines.append((
-                        (points[r][c]["x"] + radius, points[r][c]["y"] + radius),
-                        (points[r-1][c+1]["x"] + radius, points[r-1][c+1]["y"] + radius),
-                        points[r][c]["colour"]
-                    ))
-            except IndexError:
-                pass
+                    # find our if the neighbour is the same, add a line def if it is
+                    if points[r][c]["colour"] == points[r_][c_]["colour"]:
+                        lines.append((
+                            (points[r][c]["x"] + radius, points[r][c]["y"] + radius),
+                            (points[r_][c_]["x"] + radius, points[r_][c_]["y"] + radius),
+                            points[r][c]["colour"]
+                        ))
+                except IndexError:  # ignore the index errors, expected when we hit the right/bottom
+                    pass
 
     # draw our lines
+    # TODO - randomise this as the bottom right lines are always drawn over the upper left lines
     for line in lines:
         d.line(
             [
@@ -116,16 +102,21 @@ def main():
                 (line[1][0], line[1][1])
             ],
             fill=line[2],
-            width=5
+            width=line_thickness
         )
-
-    # from pprint import pprint
-    # pprint(points)
 
     # adding circles
     for row in points:
         for col in row:
-            ring(d, x=col['x'], y=col['y'], size=diameter, thickness=thickness, outline=col['colour'], fill="black")
+            ring(
+                d,
+                x=col['x'],
+                y=col['y'],
+                size=diameter,
+                thickness=thickness,
+                outline=col['colour'],
+                fill=fill
+            )
 
     img.save('out/example.png')
 
